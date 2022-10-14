3b557029e8fbe21c205f157ecda632b3d0b3ef0e
Lisandro Marchionni <djlm2140@gmail.com>
2022-10-14 20:40:51 -0300

    added env variables for production and development

--- src/app/team-data.service.ts
+++ src/app/team-data.service.ts
@@ -2,14 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Team } from './team';
+import { environment } from 'src/environments/environment';
 
-const BASE_URL = 'https://fvl-api.onrender.com/teams';
+const endpoint = `${environment.apiUrl}/teams`;
 @Injectable({
   providedIn: 'root',
 })
 export class TeamDataService {
   constructor(private http: HttpClient) {}
   getAll(): Observable<Team[]> {
-    return this.http.get<Team[]>(BASE_URL);
+    return this.http.get<Team[]>(endpoint);
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://fvl-api.onrender.com',
 };
