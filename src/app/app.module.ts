9023b16d73b203f2886a5dcbc4a5493198dd4ed1
Lisandro Marchionni <djlm2140@gmail.com>
2022-10-14 15:59:42 -0300

    First contact with Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TeamListComponent } from './team-list/team-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamListComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
