f46d00ef1ac6aebe663c901bc5af363c96d37ef6
Lisandro Marchionni <djlm2140@gmail.com>
2022-10-14 16:01:40 -0300

    Team interface

---
+++ src/app/team.ts
@@ -0,0 +1,9 @@
+export interface Team {
+  id: number;
+  name: string;
+  short_name: string;
+  country: string;
+  location: string;
+  // matchsLocal: Match[];
+  // matchsVisitante: Match[];
+}
