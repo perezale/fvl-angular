3a0c7556d370c76db8ae68d06db6d5a5621e9606
Lisandro Marchionni <djlm2140@gmail.com>
2022-10-20 15:42:59 -0300

    remove home-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './home-page/home-page.component';
 import { MatchListComponent } from './match-list/match-list.component';
 import { TeamListComponent } from './team-list/team-list.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomePageComponent,
+    redirectTo: 'teams',
+    pathMatch: 'full',
   },
   {
     path: 'teams',
